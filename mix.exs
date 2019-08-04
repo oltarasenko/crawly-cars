f684ef818fc3c505a3d1c59b773e618f48a0de41
Oleg Tarasenko <oltarasenko@gmail.com>
2019-08-04 14:48:50 +0200

    Swtich to use git version of Crawly instead pf hex

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule CarsShop.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:crawly, "~> 0.2.0"}
+      {:crawly, git: "https://github.com/oltarasenko/crawly.git", tag: "0.2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
